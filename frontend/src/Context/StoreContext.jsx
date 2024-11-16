65aabf9cb2d1b189493531f5645a9e325f818b41
Jaime Daniel Rodriguez Montaña <128983581+Jaime9745@users.noreply.github.com>
2024-11-15 21:14:32 -0500

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
-    const currency = "₹";
+    const currency = "$";
     const deliveryCharge = 50;
 
     const addToCart = async (itemId) => {
