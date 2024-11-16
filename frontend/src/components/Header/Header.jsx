87fd68fea1ffc2dfae1259c60615afb14592ef77
Jaime Rodriguez <jaimedanielmontana@gmail.com>
2024-11-15 21:04:36 -0500

    Artesanos Online

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Explora Nuestra Colección</h2>
+                <p>Descubre una diversa gama de tesoros artesanales. Nuestra misión es celebrar el arte y la tradición, ofreciendo piezas únicas que aportan cultura y creatividad a tu vida, una obra artesanal a la vez.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
